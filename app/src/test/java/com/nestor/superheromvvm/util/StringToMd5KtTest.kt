4fdc32cb124e58ad828fdc3edeb8f23c6909978a
Juan Carlos <juancarlos@Juans-MacBook-Pro.local>
2023-03-06 11:41:14 -0600

    feat: Adding hacker rank results

--- app/src/test/java/com/nestor/superheromvvm/util/StringToMd5KtTest.kt
+++ app/src/test/java/com/nestor/superheromvvm/util/StringToMd5KtTest.kt
@@ -2,6 +2,7 @@ package com.nestor.superheromvvm.util
 
 import org.junit.Assert.*
 import org.junit.Test
+import java.util.Collections
 
 class StringToMd5KtTest {
     /**
@@ -9,7 +10,21 @@ class StringToMd5KtTest {
      */
     @Test
     fun `should generate the correct hash`() {
-        //TODO: Test toMd5() extension function
+        val testing = "12345150f4cccf722f319e9f0329b039227c4fb6e067ce491e7f6ea12527452affb9f6b93ef4"
+
+        val result = testing.toMd5()
+
+        assertEquals(result, "4fcce3cac8f8af57bc5d2a6b76d78200")
+    }
+
+    @Test
+    fun `migratory birds success`() {
+        val arr = arrayOf(
+            1, 4, 4, 4, 5, 3, 3, 3
+        )
+        val result = BirdsUtils.migratoryBirds(arr)
+
+        assertEquals(result, 3)
     }
 
 }
